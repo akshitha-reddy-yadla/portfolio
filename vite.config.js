6905b8249aca70db60d1b89d7d1acd9538f12939
Akshitha Reddy <akshithareddy.yadla@gmail.com>
2025-02-17 22:47:47 +0530

    Update config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/portfolio',
   plugins: [react(), svgr()],
   define: {
     "process.env": process.env,
